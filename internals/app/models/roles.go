3f687c5d71f4dcf42eff46356c1d60e42d19a50c
Марис <70753488+KreogenGames@users.noreply.github.com>
2023-05-18 16:48:10 +0300

    hotfix

--- internals/app/models/roles.go
+++ internals/app/models/roles.go
@@ -3,5 +3,5 @@ package models
 type Role struct {
 	Id          int64  `json:"id" db:"id"`
 	RoleName    string `json:"role_name" db:"role_name"`
-	AccessLevel string `json:"access_level" db:"access_level"`
+	AccessLevel int64  `json:"access_level" db:"access_level"`
 }
